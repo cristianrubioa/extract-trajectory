cc56c1f0e085abf3d214ebea7c30e729533c5088
cristianrubioa <cristianrubioa@gmail.com>
2021-02-07 04:44:58 -0500

    Create class Extract

--- src/extract_data.py
+++ src/extract_data.py
@@ -4,38 +4,42 @@ import time
 import rospy
 from nav_msgs.msg import Odometry
 
-trajectoryFile = None
-timeStamp = None
-pitch = None
 
+class Extract:
+    def __init__(self):
+        self.trajectory_file = None
 
-def odom_callback(data):
-    timeStampNum = data.header.stamp.secs + \
-        (data.header.stamp.nsecs * 10**(-9))
-    timeStamp = str(timeStampNum)
+    def odom_callback(self, data):
+        timeStampNum = data.header.stamp.secs + \
+            (data.header.stamp.nsecs * 10**(-9))
+        timeStamp = str(timeStampNum)
 
-    tx = data.pose.pose.position.x
-    ty = data.pose.pose.position.y
-    tz = data.pose.pose.position.z
+        tx = data.pose.pose.position.x
+        ty = data.pose.pose.position.y
+        tz = data.pose.pose.position.z
 
-    qx = data.pose.pose.orientation.x
-    qy = data.pose.pose.orientation.y
-    qz = data.pose.pose.orientation.z
-    qw = data.pose.pose.orientation.w
+        qx = data.pose.pose.orientation.x
+        qy = data.pose.pose.orientation.y
+        qz = data.pose.pose.orientation.z
+        qw = data.pose.pose.orientation.w
 
-    trajectoryFile.write(timeStamp + " " + str(tx) + " " + str(ty) + " " + str(
-        tz) + " " + str(qx) + " " + str(qy) + " " + str(qz) + " " + str(qw) + "\n")
+        values = timeStamp + " " + str(tx) + " " + str(ty) + " " + str(
+            tz) + " " + str(qx) + " " + str(qy) + " " + str(qz) + " " + str(qw) + "\n"
 
+        self.trajectory_file.write(values)
+        print("[DATA] ({})".format(values))
 
-def writeFile(file):
-    # inicializa nodo ROS
-    rospy.init_node('extract_data_odometry')
-    trajectoryFile = open(file, 'w')
+    def write_file(self, file):
+        self.trajectory_file = open(file, 'w')
 
-    rospy.Subscriber('/integrated_to_init', Odometry, odom_callback)
-    rospy.spin()
-    trajectoryFile.close()
+        rospy.init_node('extract_data_odometry')
+        print(">>> Waiting to extract data ...")
+        rospy.Subscriber('/integrated_to_init', Odometry, self.odom_callback)
+        rospy.spin()
+
+        self.trajectory_file.close()
 
 
 if __name__ == '__main__':
-    writeFile('trajectory.txt')
+    e = Extract()
+    e.write_file('trajectory.txt')
